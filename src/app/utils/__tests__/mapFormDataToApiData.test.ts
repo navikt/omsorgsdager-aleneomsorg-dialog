3670754b592f9bae996cdf52d9cb0285f3db96ab
Aleksei Poroshin <aleksei.poroshin@nav.no>
2021-06-09 11:54:56 +0200

    Endret mapFormDataToApi test

--- src/app/utils/__tests__/mapFormDataToApiData.test.ts
+++ src/app/utils/__tests__/mapFormDataToApiData.test.ts
@@ -7,12 +7,13 @@ import {
     SoknadFormField,
 } from '../../types/SoknadFormData';
 import { AndreBarn } from '../../pre-common/forms/barn/types';
-import { SoknadApiData } from '../../types/SoknadApiData';
+import { ApiBarn, SoknadApiData, TidspunktForAleneomsorgApi } from '../../types/SoknadApiData';
 import { mapFormDataToApiData } from '../map-form-data-to-api-data/mapFormDataToApiData';
 
 const barnsFødselsdato = new Date(2021, 0, 20);
 const registrerteBarnMock: Barn[] = [
     { fødselsdato: barnsFødselsdato, fornavn: 'Mock', etternavn: 'Mocknes', aktørId: '84589' },
+    { fødselsdato: barnsFødselsdato, fornavn: 'Mock2', etternavn: 'Mocknes2', aktørId: '87857' },
 ];
 const andreBarnMock: AndreBarn[] = [{ id: '676478', navn: 'Jack ONeil', fnr: '18058522232' }];
 const aleneomsorgTidspunkt: AleneomsorgTidspunkt[] = [
@@ -22,8 +23,33 @@ const aleneomsorgTidspunkt: AleneomsorgTidspunkt[] = [
         dato: '2021-04-15',
     },
     { fnrId: '84589', tidspunktForAleneomsorg: TidspunktForAleneomsorgFormData.TIDLIGERE },
+    { fnrId: '87857', tidspunktForAleneomsorg: TidspunktForAleneomsorgFormData.TIDLIGERE, dato: '2021-03-12' },
 ];
-const harAleneomsorgFor = ['84589', '18058522232'];
+const harAleneomsorgFor = ['84589', '87857', '18058522232'];
+
+const mockApiBarn: ApiBarn[] = [
+    {
+        navn: 'Jack ONeil',
+        aktørId: undefined,
+        identitetsnummer: '18058522232',
+        tidspunktForAleneomsorg: TidspunktForAleneomsorgApi.SISTE_2_ÅRENE,
+        dato: '2021-04-15',
+    },
+    {
+        navn: 'Mock Mocknes',
+        aktørId: '84589',
+        identitetsnummer: undefined,
+        tidspunktForAleneomsorg: TidspunktForAleneomsorgApi.TIDLIGERE,
+        dato: undefined,
+    },
+    {
+        navn: 'Mock2 Mocknes2',
+        aktørId: '87857',
+        identitetsnummer: undefined,
+        tidspunktForAleneomsorg: TidspunktForAleneomsorgApi.TIDLIGERE,
+        dato: undefined,
+    },
+];
 const formDataMock: SoknadFormData = {
     [SoknadFormField.harForståttRettigheterOgPlikter]: true,
     [SoknadFormField.harBekreftetOpplysninger]: true,
@@ -57,7 +83,7 @@ describe('mapFormDataToApiData', () => {
         expect(resultingApiData.harBekreftetOpplysninger).toBe(formDataMock[SoknadFormField.harBekreftetOpplysninger]);
     });
 
-    it('ckeck array length', () => {
-        expect(resultingApiData.barn.length).toBe(2);
+    it('ckeck map to ApiBarn', () => {
+        expect(resultingApiData.barn).toEqual(mockApiBarn);
     });
 });
