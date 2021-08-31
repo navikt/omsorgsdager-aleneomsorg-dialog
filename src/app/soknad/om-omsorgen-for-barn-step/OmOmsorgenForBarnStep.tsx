23b94d507d00a6e12f1514fda4570d6b04c9f417
Frode Hansen <frode.hansen@bekk.no>
2021-08-31 16:04:34 +0200

    Oppdatert veileder fra common

--- src/app/soknad/om-omsorgen-for-barn-step/OmOmsorgenForBarnStep.tsx
+++ src/app/soknad/om-omsorgen-for-barn-step/OmOmsorgenForBarnStep.tsx
@@ -59,7 +59,7 @@ const OmOmsorgenForBarnStep = ({ barn }: Props) => {
 
     return (
         <SoknadFormStep id={StepID.OM_OMSORGEN_FOR_BARN} onStepCleanup={cleanupOmOmsorgenForBarnStep}>
-            <CounsellorPanel switchToPlakatOnSmallScreenSize={true}>
+            <CounsellorPanel>
                 <p>
                     <FormattedMessage id="step.om-omsorgen-for-barn.stepIntro.1" />
                 </p>
