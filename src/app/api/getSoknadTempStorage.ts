7d5f6160856505fc85c63d0f4253dc2414fc7491
Aleksei Poroshin <aleksei.poroshin@nav.no>
2021-09-29 10:52:42 +0200

    Bug fiks3

--- src/app/api/getSoknadTempStorage.ts
+++ src/app/api/getSoknadTempStorage.ts
@@ -9,7 +9,6 @@ const getSoknadTempStorage = async (): Promise<SoknadTempStorageRemoteData> => {
     try {
         const { data } = await api.get<SoknadTempStorageData>(ApiEndpoint.mellomlagring);
         console.log('Mellomlagring: ', data);
-        console.log(data);
         return Promise.resolve(success(data));
     } catch (error) {
         return Promise.reject(failure(error));
