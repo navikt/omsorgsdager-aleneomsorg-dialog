8578e97b306f74ad3c5d92e1ac3ed73c33a3becd
Aleksei Poroshin <aleksei.poroshin@nav.no>
2022-04-07 10:29:08 +0200

    Fiks intro

--- src/app/pages/intro-page/IntroForm.tsx
+++ src/app/pages/intro-page/IntroForm.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { FormattedMessage, useIntl } from 'react-intl';
+import { useIntl } from 'react-intl';
 import { YesOrNo } from '@navikt/sif-common-core/lib/types/YesOrNo';
 import intlHelper from '@navikt/sif-common-core/lib/utils/intlUtils';
 import { IntroFormData, IntroFormField, introFormInitialValues } from './introFormConfig';
 import FormQuestion from '@navikt/sif-common-soknad/lib/form-question/FormQuestion';
-import { getTypedFormComponents, UnansweredQuestionsInfo } from '@navikt/sif-common-formik';
+import { getTypedFormComponents } from '@navikt/sif-common-formik';
 import getIntlFormErrorHandler from '@navikt/sif-common-formik/lib/validation/intlFormErrorHandler';
 import { ValidationError } from '@navikt/sif-common-formik/lib/validation/types';
 import { getYesOrNoValidator } from '@navikt/sif-common-formik/lib/validation';
@@ -23,38 +23,48 @@ const IntroForm = ({ onValidSubmit }: Props) => {
             onSubmit={() => {
                 onValidSubmit();
             }}
-            renderForm={({ values: { erSokerlestInformasjonen, erSokerAleneOmsorg } }) => {
+            renderForm={({ values: { erSokerlestInformasjonen } }) => {
                 const lestInformasjonenBesvart = erSokerlestInformasjonen === YesOrNo.YES;
-                const AleneOmsorgBesvart = erSokerAleneOmsorg === YesOrNo.YES;
-                const kanFortsette: boolean = lestInformasjonenBesvart && AleneOmsorgBesvart;
 
                 return (
                     <IntroFormComponents.Form
                         includeValidationSummary={true}
-                        includeButtons={kanFortsette}
                         formErrorHandler={getIntlFormErrorHandler(intl, 'introForm.validation')}
-                        submitButtonLabel={intlHelper(intl, 'introForm.start')}
-                        noButtonsContentRenderer={() =>
-                            kanFortsette ? undefined : (
-                                <UnansweredQuestionsInfo>
-                                    <FormattedMessage id="page.form.ubesvarteSpørsmålInfo" />
-                                </UnansweredQuestionsInfo>
-                            )
-                        }>
+                        submitButtonLabel={intlHelper(intl, 'introForm.start')}>
                         <FormQuestion
                             legend={intlHelper(intl, `introForm.erSokerlestInformasjonen.spm`)}
                             name={IntroFormField.erSokerlestInformasjonen}
-                            validate={getYesOrNoValidator()}
-                            showStop={erSokerlestInformasjonen === YesOrNo.NO}
-                            stopMessage={intlHelper(intl, 'introForm.erSokerlestInformasjonen.stopMessage')}
+                            validate={(value) => {
+                                const error = getYesOrNoValidator()(value);
+                                if (error) {
+                                    return error;
+                                }
+                                if (value === YesOrNo.NO) {
+                                    return {
+                                        key: 'introForm.validation.erSokerlestInformasjonen.nei',
+                                        keepKeyUnaltered: true,
+                                    };
+                                }
+                                return undefined;
+                            }}
                         />
                         {lestInformasjonenBesvart && (
                             <FormQuestion
                                 legend={intlHelper(intl, `introForm.erSokerAleneOmsorg.spm`)}
                                 name={IntroFormField.erSokerAleneOmsorg}
-                                validate={getYesOrNoValidator()}
-                                showStop={erSokerAleneOmsorg === YesOrNo.NO}
-                                stopMessage={intlHelper(intl, 'introForm.erSokerAleneOmsorg.stopMessage')}
+                                validate={(value) => {
+                                    const error = getYesOrNoValidator()(value);
+                                    if (error) {
+                                        return error;
+                                    }
+                                    if (value === YesOrNo.NO) {
+                                        return {
+                                            key: 'introForm.validation.erSokerAleneOmsorg.nei',
+                                            keepKeyUnaltered: true,
+                                        };
+                                    }
+                                    return undefined;
+                                }}
                             />
                         )}
                     </IntroFormComponents.Form>
