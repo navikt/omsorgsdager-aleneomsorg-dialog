7e869d46a54032404042a940da1d30c75924aa26
Frode Hansen <frode.hansen@bekk.no>
2022-12-07 15:54:57 +0100

    Jeg bekrefter tekst

--- src/app/soknad/velkommen-page/velkommenPageMessages.ts
+++ src/app/soknad/velkommen-page/velkommenPageMessages.ts
@@ -22,14 +22,14 @@ export const velkommenPageMessages: MessageFileFormat = {
         'page.velkommen.omSøknaden.tittel': 'Om søknaden',
         'page.velkommen.omSøknaden.1': 'Du får veiledning underveis i søknaden om hva du skal fylle ut, og hvordan.',
         'page.velkommen.omSøknaden.2':
-            'Vi tar vare på svarene dine i 72 timer. Så, hvis du innenfor den tiden for eksempel vil ta en pause eller blir automatisk logget ut, fortsetter du der du var når du kommer tilbake.',
+            'Vi tar vare på svarene dine i 72 timer. Hvis du innenfor den tiden for eksempel vil ta en pause eller blir automatisk logget ut, fortsetter du der du var når du kommer tilbake.',
         'page.velkommen.omSøknaden.3':
             'Du må svare på alle spørsmålene for å kunne gå videre. Hvis du mangler etterspurt dokumentasjon, kan du ettersende det så snart du kan.',
         'page.velkommen.omSøknaden.4': 'Om hvordan vi innhenter opplysninger om deg',
-        'page.velkommen.form.bekreftLabel': 'Jeg bekrefter at jeg har lest og forstått',
+        'page.velkommen.form.bekreftLabel': 'Jeg bekrefter at jeg har forstått mitt ansvar som søker',
         'page.velkommen.form.ansvar.tittel': 'Ditt ansvar som søker',
         'page.velkommen.form.ansvar.list.1':
-            'Jeg forstår at hvis jeg gir uriktige opplysninger, kan det få konsekvenser for retten min til det jeg søker om',
+            'Jeg forstår at hvis jeg gir uriktige opplysninger, kan det få konsekvenser for retten min til det jeg søker om.',
         'page.velkommen.form.ansvar.list.2.1': 'Jeg har lest og forstått det som står på',
         'page.velkommen.form.ansvar.list.2.2': 'nav.no/rett og plikt',
         'page.velkommen.form.ansvar.list.2.3': '.',
